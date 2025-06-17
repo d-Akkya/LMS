e8d734c64adb2a47936a600f139589836b97ea6b
d-Akkya <akhileshbamane26@gmail.com>
2025-06-17 17:51:02 +0530

    initialize the project with LoginPage

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,142 @@
+import { AppWindowIcon, CodeIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+const Login = () => {
+  const [signupInput, setSignupInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+
+  const changeInputHandler = (e, type) => {
+    const { name, value } = e.target;
+    if (type === "signup") {
+      setSignupInput({ ...signupInput, [name]: value });
+    } else {
+      setLoginInput({ ...loginInput, [name]: value });
+    }
+  };
+
+  const handleRegistration = (type) => {
+    const inputData = type === "signup" ? signupInput : loginInput;
+    console.log(inputData);
+  };
+
+  return (
+    <div className="flex items-center w-full justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Tabs defaultValue="account">
+          <TabsList className="w-full">
+            <TabsTrigger value="signup">Signup</TabsTrigger>
+            <TabsTrigger value="login">Login</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signup">
+            <Card>
+              <CardHeader>
+                <CardTitle>Signup</CardTitle>
+                <CardDescription>
+                  Create a new account and click signup when you&apos;re done.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="tabs-demo-name">Name</Label>
+                  <Input
+                    type="text"
+                    name="name"
+                    value={signupInput.name}
+                    onChange={(e) => changeInputHandler(e, "signup")}
+                    placeholder="eg. John Developer"
+                    required="true"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="tabs-demo-username">Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    value={signupInput.email}
+                    onChange={(e) => changeInputHandler(e, "signup")}
+                    placeholder="eg. johndeveloper@gmail.com"
+                    required="true"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="tabs-demo-username">Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={signupInput.password}
+                    onChange={(e) => changeInputHandler(e, "signup")}
+                    placeholder="eg. xyz"
+                    required="true"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button onClick={() => handleRegistration("signup")}>
+                  Signup
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="login">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Login your password here. After signup, you&apos;ll be logged
+                  in.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="tabs-demo-current">Email</Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    value={loginInput.email}
+                    onChange={(e) => changeInputHandler(e, "login")}
+                    placeholder="eg. johndeveloper@gmail.com"
+                    required="true"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="tabs-demo-new">Password</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    value={loginInput.password}
+                    onChange={(e) => changeInputHandler(e, "login")}
+                    placeholder="eg. xyz"
+                    required="true"
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button onClick={() => handleRegistration("login")}>
+                  Login
+                </Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+export default Login;
