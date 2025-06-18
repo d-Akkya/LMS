0808ac63de0fdff690bd976f7642a76cf4506a96
d-Akkya <akhileshbamane26@gmail.com>
2025-06-18 19:52:22 +0530

    feat(api): implement auth API with login/register mutations

---
+++ client/src/features/api/authApi.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { userLoggedIn } from "../authSlice.js";
+
+const USER_API = "http://localhost:5000/api/v1/user/";
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: USER_API,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: (inputData) => ({
+        url: "register",
+        method: "POST",
+        body: inputData,
+      }),
+    }),
+
+    loginUser: builder.mutation({
+      query: (credentials) => ({
+        url: "login",
+        method: "POST",
+        body: credentials,
+      }),
+      async onQueryStarted(_, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(userLoggedIn({ user: result.data.user }));
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }),
+  }),
+});
+
+export const { useRegisterUserMutation, useLoginUserMutation } = authApi;
