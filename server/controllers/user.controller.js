0ec63d0f18a998021ee0f3e4e2982ab65f96f4f2
d-Akkya <akhileshbamane26@gmail.com>
2025-06-18 13:09:55 +0530

    feat(auth): implement register/login with bcryptjs + JWT

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,78 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required.",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists with this email.",
+      });
+    }
+    // before storing the data into db secure the password
+    const hashPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Account created successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required.",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+
+    // using cookie session token for the authentication
+    generateToken(res, user, `Welcome back ${user.name}`);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Login",
+    });
+  }
+};
