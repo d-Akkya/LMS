eda5d9b576e5c5cde50e475ccbaec94e34e91ad2
d-Akkya <akhileshbamane26@gmail.com>
2025-06-19 14:42:05 +0530

    feat(navbar): implement account dropdown menu and theme icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,17 @@
 import { School } from "lucide-react";
 import React from "react";
-
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import DarkMode from "@/DarkMode";
 
 const Navbar = () => {
   const user = true;
@@ -12,6 +23,45 @@ const Navbar = () => {
         <div className="flex items-center gap-2">
           <School size={30} />
           <h1 className="hidden md:block font-extrabold text-2xl">SkillNova</h1>
+        </div>
+        {/* User icon and dark mode icon */}
+        <div className="flex items-center gap-8">
+          <DarkMode />
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="rounded-md h-[34px] w-[34px] cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/evilrabbit.png"
+                    alt="@evilrabbit"
+                  />
+                  <AvatarFallback>ER</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56" align="start">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Continue Learning</DropdownMenuItem>
+                  <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Button variant="outline" className="w-[78px]">
+                Signup
+              </Button>
+              <Button
+                variant="outline"
+                className="w-[78px] bg-black text-white"
+              >
+                Login
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
