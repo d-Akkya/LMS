06af76fcf0a3cc1b17379499da23a0fc6ff13302
d-Akkya <akhileshbamane26@gmail.com>
2025-06-23 21:03:19 +0530

    refactor(routes): add PUT /profile/update endpoint

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,8 +4,10 @@ import {
   login,
   getUserProfile,
   logout,
+  updateProfile,
 } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
@@ -13,5 +15,6 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile").get(isAuthenticated, getUserProfile);
+router.route("/profile/update").put(isAuthenticated, upload.single("profilePhoto"),updateProfile);
 
 export default router;
