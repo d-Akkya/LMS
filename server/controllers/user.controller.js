22be94ff0e6ecf541cd333798998ef3b4147ceae
d-Akkya <akhileshbamane26@gmail.com>
2025-06-23 16:05:23 +0530

    feat(profile): add getUserProfile and logout logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -69,10 +69,47 @@ export const login = async (req, res) => {
     generateToken(res, user, `Welcome back ${user.name}`);
   } catch (error) {
     console.log(error);
-
     return res.status(500).json({
       success: false,
       message: "Failed to Login",
+    });
+  }
+};
+
+export const logout = async (_, res) => {
+  try {
+    return res
+      .status(200)
+      .cookie("token", "", { maxAge: 0 })
+      .json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Logout",
+    });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const userId = req.id;
+    const user = await User.findById(userId).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      user
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to get user profile",
     });
   }
 };
