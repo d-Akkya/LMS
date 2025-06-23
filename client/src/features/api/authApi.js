ce1aa35614f7a722f1bb69b05890c012e9fc4f0e
d-Akkya <akhileshbamane26@gmail.com>
2025-06-23 16:07:56 +0530

    feat(auth): add loadUser query endpoint

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -33,7 +33,18 @@ export const authApi = createApi({
         }
       },
     }),
+    
+    loadUser: builder.query({
+      query: () => ({
+        url: "profile",
+        method: "GET",
+      }),
+    }),
   }),
 });
 
-export const { useRegisterUserMutation, useLoginUserMutation } = authApi;
+export const {
+  useRegisterUserMutation,
+  useLoginUserMutation,
+  useLoadUserQuery,
+} = authApi;
