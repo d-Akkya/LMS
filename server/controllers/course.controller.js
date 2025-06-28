b6d4ec34bf6d64e528112dc116130b71ce20b2fc
d-Akkya <akhileshbamane26@gmail.com>
2025-06-28 22:29:02 +0530

    feat(course): implement createCourse controller

---
+++ server/controllers/course.controller.js
@@ -0,0 +1,31 @@
+import { Course } from "../models/course.model.js";
+
+export const createCourse = async (req, req) => {
+  try {
+    const { courseTitle, category } = req.body;
+    if (!courseTitle || !category) {
+      return res.status(400).json({
+        success: false,
+        message: "Course title and category are required.",
+      });
+    }
+
+    const course = new Course.create({
+      courseTitle,
+      category,
+      creator: req.id,
+    });
+
+    return res.status(201).json({
+      course,
+      success: true,
+      message: "Course created.",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create course",
+    });
+  }
+};
