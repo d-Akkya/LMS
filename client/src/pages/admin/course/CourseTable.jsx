6186962d17c6cb88d2d02bfc2126c956b36d36de
d-Akkya <akhileshbamane26@gmail.com>
2025-07-02 19:57:19 +0530

    feat(ui): add skeleton loading for course table

--- client/src/pages/admin/course/CourseTable.jsx
+++ client/src/pages/admin/course/CourseTable.jsx
@@ -64,6 +64,7 @@ const CourseTable = () => {
   const { data, isLoading } = useGetCreatorCourseQuery();
   const navigate = useNavigate();
 
+  if (isLoading) return <CourseTableSkeleton />;
   return (
     <div>
       <Button onClick={() => navigate("create")} className={"cursor-pointer"}>
@@ -109,3 +110,53 @@ const CourseTable = () => {
 };
 
 export default CourseTable;
+
+const CourseTableSkeleton = () => {
+  return (
+    <div>
+      {/* Add Course Button */}
+      <Skeleton className="h-10 w-36 mb-4" />
+
+      <Table>
+        <TableCaption>
+          <Skeleton className="h-4 w-48" />
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Price</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Title</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {/* Generate skeleton rows */}
+          {[...Array(5)].map((_, index) => (
+            <TableRow key={index}>
+              <TableCell className="font-medium">
+                <Skeleton className="h-4 w-16" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-20" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-32" />
+              </TableCell>
+              <TableCell className="text-right">
+                <Skeleton className="h-4 w-16" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">
+              <Skeleton className="h-4 w-20" />
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  );
+};
