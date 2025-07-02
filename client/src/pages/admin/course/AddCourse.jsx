85c6a6200fe0bd8f6c034b49ad6819dcfbd38db8
d-Akkya <akhileshbamane26@gmail.com>
2025-07-02 19:39:59 +0530

    feat(courses): redirect to course list after creation

--- client/src/pages/admin/course/AddCourse.jsx
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -37,6 +37,7 @@ const AddCourse = () => {
   useEffect(() => {
     if (isSuccess) {
       toast.success(data?.message || "Course created successfully.");
+      navigate("/admin/course");
     }
   }, [isSuccess, error]);
 
