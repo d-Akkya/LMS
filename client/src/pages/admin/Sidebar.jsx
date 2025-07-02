9a5e7c5eea1698584745137177d7a47a0ff3609b
d-Akkya <akhileshbamane26@gmail.com>
2025-07-02 19:36:40 +0530

    feat(sidebar): implement active link styling with NavLink

--- client/src/pages/admin/Sidebar.jsx
+++ client/src/pages/admin/Sidebar.jsx
@@ -1,20 +1,34 @@
 import { ChartNoAxesColumn, SquareLibrary } from "lucide-react";
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="flex">
       <div className="hidden lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-gray-300 dark:border-gray-700 bg-[#f0f0f0] p-5 sticky top-0 h-screen">
         <div className="mt-20 space-y-4">
-          <Link to="dashboard" className="flex items-center gap-2">
+          <NavLink
+            to="dashboard"
+            className={({ isActive }) =>
+              `flex items-center gap-2 ${
+                isActive ? "text-blue-600" : "text-gray-900 dark:text-gray-300"
+              }`
+            }
+          >
             <ChartNoAxesColumn size={22} />
             <h1>Dashboard</h1>
-          </Link>
-          <Link to="course" className="flex items-center gap-2">
+          </NavLink>
+          <NavLink
+            to="course"
+            className={({ isActive }) =>
+              `flex items-center gap-2 ${
+                isActive ? "text-blue-600" : "text-gray-900 dark:text-gray-300"
+              }`
+            }
+          >
             <SquareLibrary size={22} />
             <h1>Courses</h1>
-          </Link>
+          </NavLink>
         </div>
       </div>
       <div className="flex-1 md:p-24 p-2 bg-white">
