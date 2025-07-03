18f66cd715dc703c455a83f6fea9ee4fe0b9731e
d-Akkya <akhileshbamane26@gmail.com>
2025-07-03 22:57:53 +0530

    feat(courses): initialize edit course page with base styling

---
+++ client/src/pages/admin/course/EditCourse.jsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const EditCourse = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex-1">
+      <div className="flex items-center justify-between mb-5">
+        <h1 className="font-bold text-xl">
+          Add detail information regarding course
+        </h1>
+        <Button
+          variant="link"
+          className={"hover:text-blue-600 cursor-pointer"}
+          onClick={() => navigate("lecture")}
+        >
+          Go to lectures page
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default EditCourse;
