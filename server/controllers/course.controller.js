6a9916e68a45905182bb2f97acb7b11f09b698b2
d-Akkya <akhileshbamane26@gmail.com>
2025-07-07 22:52:32 +0530

    feat(courses): implement editCourse controller for course updates

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,4 +1,5 @@
 import { Course } from "../models/course.model.js";
+import { deleteMediaFromCloudinary, uploadMedia } from "../utils/cloudinary.js";
 
 export const createCourse = async (req, res) => {
   try {
@@ -50,6 +51,66 @@ export const getCreatorCourses = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Failed to fetch courses.",
+    });
+  }
+};
+
+export const editCourse = async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+    const {
+      courseTitle,
+      subtitle,
+      description,
+      category,
+      courseLevel,
+      coursePrice,
+    } = req.body;
+    const thumbnail = req.file;
+    
+    let course = await Course.findById(courseId);
+    if (!course) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found!",
+      });
+    }
+
+    // get course thumbnail if already exist and delete it from cloudinary
+    let courseThumbnail;
+    if (thumbnail) {
+      if (course.courseThumbnail) {
+        const publicId = course.courseThumbnail.split("/").pop().split(".")[0];
+        await deleteMediaFromCloudinary(publicId); // delete old image
+      }
+      // upload new thumbnail on cloudinary
+      courseThumbnail = await uploadMedia(thumbnail.path);
+    }
+
+    // update course data
+    const updateData = {
+      courseTitle,
+      subtitle,
+      description,
+      category,
+      courseLevel,
+      coursePrice,
+      courseThumbnail: courseThumbnail?.secure_url,
+    };
+
+    course = await Course.findByIdAndUpdate(courseId, updateData, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      course,
+      message: "Course updated successfully.",
+    });
+  } catch (error) {
+    console.log;
+    return res.status(500).json({
+      success: false,
+      message: "Failed to edit course.",
     });
   }
 };
